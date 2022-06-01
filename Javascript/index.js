cff2ea2dd2ab35d9f276fb0cacfccdbc3d27456a
Pedro <pedrohenriquecamposmendes@hotmail.com>
2022-05-31 22:20:08 -0300

    js

---
+++ Javascript/index.js
@@ -0,0 +1,16 @@
+//document.getElementById("count").innerText = 5
+//let count = 0
+//console.log(count)
+
+//let myAge = 20 
+//let idadeCachorro = idadeHumana * 7
+
+//console.log(idadeCachorro)
+
+let count = 0
+
+function increment() {
+    console.log("The button was clicked!")
+}
+
+console.log(increment())
