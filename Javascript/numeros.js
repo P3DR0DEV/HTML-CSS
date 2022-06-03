4bd521bed3ae7e809d1e981894d1d90eed8782db
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2022-06-03 17:37:39 -0300

    Numeros update

--- Javascript/numeros.js
+++ Javascript/numeros.js
@@ -4,4 +4,14 @@ console.log(num1.toFixed(2)) // 2 casas decimais
 
 console.log(num1.toFixed(2).replace('.', ',')) // muda o ponto para virgula
 
-console.log(num1.LocaleString('pt-BR', {style: 'currency', currency: 'BRL'}))
+// console.log(num1.LocaleString('pt-BR', {style: 'currency', currency: 'BRL'}))
+
+let numero = 1 
+
+n++ // n + 2 
+n-- // n - 1 
+
+// para representar "and" utilizamos && ou seja, sim && sim == sim; sim && não == não
+// para representar "or" utilizamos || ou seja, sim || sim == sim; sim || não == sim; não || não == não 
+
+
