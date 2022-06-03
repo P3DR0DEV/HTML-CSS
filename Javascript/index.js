e0705f461fbd8725fef982483a6109e4da617d89
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2022-06-03 16:35:37 -0300

    update index

--- Javascript/index.js
+++ Javascript/index.js
@@ -22,7 +22,8 @@ num2 = Number.parseInt(num2) // ou Number(num2)
 let sum = num1 +num2
 
 function somar() {
-    alert(`${nome}, a soma dos 2 números digitados equivale a: ${sum}`) // Janela de alerta
+    alert(`${nome}, a soma dos 2 números digitados equivale a: ${sum}`)
+    //alert(`${sum.localeString('pt-BR', {style: 'currency', currency: 'BRL'})}`) // Janela de alerta
 }
 
 increment()
